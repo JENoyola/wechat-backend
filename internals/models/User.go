9d42c0c840fd6d374526448a1f80f0c81bcfeefe
GeorgeHN666 <jorge1029hs@gmail.com>
2024-09-12 20:20:55 -0500

    fix:GH: Fixing tag on user models
    Bson tag fixed for user models, inserting user model constants

--- internals/models/User.go
+++ internals/models/User.go
@@ -6,12 +6,18 @@ import (
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
+// CONSTANTS
+const (
+	CODE_NOT_VALID = 46
+	CODE_VALID     = 65
+)
+
 // User structure that onlines the user
 type User struct {
 	ID        primitive.ObjectID `json:"_id" bson:"_id"`
 	Name      string             `json:"name" bson:"name"`
 	Email     string             `json:"email" bson:"email"`
 	ValidCode int                `json:"valid_code" bson:"valid_code"`
-	TempCode  int                `json:"temp_code"`
+	TempCode  int                `json:"temp_code" bson:"temp_code"`
 	CreatedAt time.Time          `json:"created_at" bson:"created_at"`
 }
